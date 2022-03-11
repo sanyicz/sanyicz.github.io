9279a6eb4e6e4c33734f5763aa1fede272b9a8bf
Sándor Czimmer <czmmrsndr@gmail.com>
2022-03-12 00:11:43 +0100

    Update nav2.js

--- nav2.js
+++ nav2.js
@@ -5,20 +5,8 @@ fetch("nav2.html")
 	let oldElement = document.querySelector("script#menu2");
 	let newElement = document.createElement("div");
 	newElement.innerHTML = text;
+	let attr = document.createAttribute("id");
+	attr.value = "menu2";
+	newElement.setAttributeNode(attr);
 	oldElement.parentNode.replaceChild(newElement, oldElement);
 })
-
-
-
-//set language
-languageCode = localStorage.getItem("actualLanguageCode");
-var all = document.getElementsByTagName("*");
-for (var i=1, max=all.length; i < max; i++) { //i=0 is the <html lang="en"> element
-	element = all[i];
-	if (element.hasAttribute("lang")) {
-		//console.log('hasAttribute("lang"):', element);
-		var attr = element.getAttribute("lang");
-		if (attr == languageCode) {element.style.display = "";}
-		else {element.style.display = "none";};
-	};
-};
